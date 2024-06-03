a4abaaa700f0c31867b687b9b6b6f12a6b56cceb
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-06-03 13:13:51 -0700

    Change js file to mjs for node

---
+++ hashmap.mjs
@@ -0,0 +1,28 @@
+function HashMap() {
+    let buckets = [];
+    let capacity = buckets.length;
+    let load_factor = 0.75;
+
+    if (index < 0 || index >= capacity) {
+        throw new Error("Trying to access index out of bound");
+    }
+
+    const hash = function(key) {
+        let hashCode = 0;
+
+        const primeNumber = 31;
+        for (let i = 0; i < key.length; i++) {
+            hashCode = primeNumber * hashCode + key.charCodeAt(i);
+            hashCode %= 16;
+        }
+
+        return hashCode;
+    }
+
+    const set = function(key, value) {
+
+    }
+
+
+    return { hash }
+}
