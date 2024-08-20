18f892e4990377c082ce967603d0d7680a174a38
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2024-08-19 19:40:24 -0700

    Modify linkedList for simple key location

--- linkedList.mjs
+++ linkedList.mjs
@@ -1,139 +1,157 @@
-export function linkedList () {
-    let first = null;
-    
-    let length = 0;
+export function linkedList() {
+  let first = null;
 
-    const head = () => {return first};
+  let length = 0;
 
-    const tail = function() {
-        if (first) {
-            let subject = first;
-            while (subject.next !== null) {
-                subject = subject.next;
-            }
-    
-            return subject;
-        } else {
-            return null;
-        }
-    };
+  const head = () => {
+    return first;
+  };
 
-    const append = function(value) {
-        const newLast = node (value, null);
-        if (!first) {
-            first = newLast;
-        } else {
-            tail().next = newLast;
-        }
+  const tail = function () {
+    if (first) {
+      let subject = first;
+      while (subject.next !== null) {
+        subject = subject.next;
+      }
 
-        length += 1;
-        
+      return subject;
+    } else {
+      return null;
+    }
+  };
+
+  const append = function (key, value) {
+    const newLast = node(key, value, null);
+    if (!first) {
+      first = newLast;
+    } else {
+      tail().next = newLast;
     }
 
-    const prepend = function(value) { 
-        const newFirst = node (value, null);
+    length += 1;
+  };
 
-        if (first) {
-            newFirst.next = first; 
-        } 
+  const prepend = function (key, value) {
+    const newFirst = node(key, value, null);
 
-        first = newFirst;
-        length += 1;
+    if (first) {
+      newFirst.next = first;
     }
 
-    const size = () => {return length};
+    first = newFirst;
+    length += 1;
+  };
 
-    const at = function(index) {
-        if (first && index > 0) {
-            let index2 = 1;
-            let subject = first;
+  const size = () => {
+    return length;
+  };
 
-            while (index !== index2) {
-                index2++;
-                subject = subject.next
-            }
+  const at = function (index) {
+    if (first && index > 0) {
+      let index2 = 1;
+      let subject = first;
 
-            return subject;
+      while (index !== index2) {
+        index2++;
+        subject = subject.next;
+      }
+
+      return subject;
+    } else {
+      return null;
+    }
+  };
+
+  const pop = function () {
+    let penult = null;
+
+    if (length === 1) {
+      first = penult;
+    } else {
+      penult = list.at(length - 1);
+      penult.next = null;
+    }
+
+    length -= 1;
+  };
+
+  const contains = function (value) {
+    if (first) {
+      let subject = first;
+
+      while (value !== subject.value) {
+        if (subject.next === null) {
+          return false;
         } else {
-            return null;
+          subject = subject.next;
         }
+      }
+
+      return true;
+    } else {
+      return "This linked list is empty.";
     }
+  };
 
-    const pop = function() {
-        let penult = null;
+  const find = function (value) {
+    if (first) {
+      let index = 1;
+      let subject = first;
 
-        if (length === 1) {
-            first = penult;
+      while (subject.value !== value) {
+        if (subject.next === null) {
+          return null;
         } else {
-            penult = list.at(length - 1);
-            penult.next = null;
+          index++;
+          subject = subject.next;
         }
-        
-        length -= 1;
+      }
+
+      return index;
+    } else {
+      return null;
     }
+  };
 
-    const contains = function(value) {
-        if (first) {
-            let subject = first;
+  const toString = function () {
+    if (first) {
+      let subject = first;
+      let listString = `(${subject.value})`;
 
-            while (value !== subject.value) {
-                if (subject.next === null) {
-                    return false;
-                } else {
-                    subject = subject.next;
-                }
-            }
+      while (subject.next !== null) {
+        listString = listString + " -> " + `(${subject.next.value})`;
+        subject = subject.next;
+      }
 
-            return true;
-        } else {
-            return "This linked list is empty.";
-        }
+      return (listString += " -> null");
+    } else {
+      return null;
     }
-    
-    const find = function(value) {
-        if (first) {
-            let index = 1;
-            let subject = first;
+  };
 
-            while (subject.value !== value) {
-                if (subject.next === null) {
-                    return null;
-                } else {
-                    index++;
-                    subject = subject.next;
-                }
-            }
-
-            return index;
-        } else {
-            return null;
-        }
-    }
-
-    const toString = function() {
-        if (first) {
-            let subject = first;
-            let listString = `(${subject.value})`;
-    
-            while (subject.next !== null) {
-                listString = listString + " -> " + `(${subject.next.value})`;
-                subject = subject.next;
-            }
-
-            return listString += " -> null";
-        } else {
-            return null;
-        }
-    }
-
-    return { append, prepend, size, head, tail, at, pop, contains, find, toString };
+  return {
+    append,
+    prepend,
+    size,
+    head,
+    tail,
+    at,
+    pop,
+    contains,
+    find,
+    toString,
+  };
 }
 
-export function node (data, data2) {
-    const value = data;
-    const next = data2;
+export function node(key, value, nextVal) {
+  const nodeKey = key;
+  const nodeVal = value;
+  const obj = {};
 
-    return { value, next };
+  obj[nodeKey] = nodeVal;
+  obj.next = nextVal;
+
+  return obj;
 }
 
 const list = linkedList();
@@ -144,8 +162,3 @@ list.prepend(value);
 console.log(list.head());
 
 */
-
-
-
-
-
